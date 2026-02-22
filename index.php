cb93788143ba99b139c649bd1f171a613fcad6c2
htm823 <choco.cacao31@gmail.com>
2026-02-22 09:24:05 +0900

    feat: add server-side validation for name and url

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +11,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playwrite+AT:ital,wght@0,100..400;1,100..400&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style.min.css">
 	<title>Savelet</title>
 </head>
 
@@ -19,14 +23,18 @@
 	</header>
 	<main>
 		<div class="save container">
-			<form class="save__form" action="save.php" method="POST">
+			<div class="save__description">
+				<p class="save__text">Save and organize your favorite websites in one place.</p>
+			</div>
+			<form id="save-form" class="save__form" action="save.php" method="POST">
+				<input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token'] ?>">
 				<div class="save__field">
 					<label for="name" class="save__label form-label">Name</label>
-					<input id="name" class="save__input" name="name" type="text" placeholder="My Favorite">
+					<input id="name" class="save__input" name="name" type="text" maxlength="40" placeholder="My Favorite Website">
 				</div>
 				<div class="save__field">
 					<label for="url" class="save__label form-label">URL</label>
-					<input id="url" class="save__input" name="url" type="url" placeholder="https://my-favorite.com">
+					<input id="url" class="save__input" name="url" type="url" maxlength="2048" placeholder="https://my-favorite-website.com">
 				</div>
 				<button id="save-btn" class="save__btn btn" type="submit">Save</button>
 			</form>
--- save.php
+++ save.php
@@ -1,9 +1,44 @@
 <?php
 require 'db.php';
 
-$name = $_POST['name'];
-$url  = $_POST['url'];
+// CSRF Token
+session_start();
 
+if (
+	!isset($_POST['csrf_token'], $_SESSION['csrf_token']) ||
+	!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])
+) {
+	return;
+}
+
+// Validation
+$name = trim($_POST['name'] ?? '');
+$url  = trim($_POST['url'] ?? '');
+
+if (!$name || !$url) {
+	return;
+}
+
+$nameLength = mb_strlen($name);
+if ($nameLength > 40) {
+	return;
+}
+
+if (mb_strlen($url) > 2048) {
+	return;
+}
+
+if (!filter_var($url, FILTER_VALIDATE_URL)) {
+	return;
+}
+
+$scheme           = parse_url($url, PHP_URL_SCHEME);
+$allowedProtocols = ['http', 'https'];
+if (!in_array($scheme, $allowedProtocols, true)) {
+	return;
+}
+
+// DB Insert
 $sql  = 'INSERT INTO savelet (name, url) VALUES (:name, :url)';
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
@@ -12,3 +47,4 @@ $stmt->execute([
 ]);
 
 header('Location: index.php');
+exit;
